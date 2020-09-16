9d26b28b6762b31013ab2b35223a4913f02d6160
Andreas Dolk <andreas@dolk.de>
2020-09-16 08:59:27 +0200

    Updates the example code, now uses the ext lib by default

--- example/preload.js
+++ example/preload.js
@@ -8,8 +8,8 @@ window.addEventListener('DOMContentLoaded', () => {
   // It does not make sense to use the custom titlebar on macOS where
   // it only tries to simulate what we get with the normal behavior anyway.
   if (process.platform !== 'darwin') {
-    //const customTitlebar = require('custom-electron-titlebar');
-    const customTitlebar = require('../dist'); // Delete this line and uncomment top line
+    const customTitlebar = require('custom-electron-titlebar');
+    // const customTitlebar = require('../dist');  // for local library development and testing only
     new customTitlebar.Titlebar({
       backgroundColor: customTitlebar.Color.fromHex('#2f3241'),
       icon: url.format(path.join(__dirname, '/images', '/icon.png')),
