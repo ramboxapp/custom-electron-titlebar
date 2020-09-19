c7037c4cf9d40da18612cbd2208073dca23b179d
Andreas Dolk <andreas@dolk.de>
2020-09-19 21:21:14 +0200

    Updates example to show a menu

--- example/preload.js
+++ example/preload.js
@@ -1,18 +1,63 @@
 // All of the Node.js APIs are available in the preload process.
 // It has the same sandbox as a Chrome extension.
+
 const path = require('path');
 const url = require('url');
+const {isMacintosh} = require('custom-electron-titlebar')
+//const {isMacintosh} = require('../dist')
+const {Menu, MenuItem} = require('@treverix/remote')
 
 window.addEventListener('DOMContentLoaded', () => {
 
   // It does not make sense to use the custom titlebar on macOS where
   // it only tries to simulate what we get with the normal behavior anyway.
-  if (process.platform !== 'darwin') {
+  if (!isMacintosh) {
     const customTitlebar = require('custom-electron-titlebar');
-    // const customTitlebar = require('../dist');  // for local library development and testing only
+    //const customTitlebar = require('../dist');  // for local library development and testing only
+
+    // add a menu
+    const menu = new Menu();
+    menu.append(new MenuItem({
+      label: 'Item 1',
+      click: () => console.log('Click on submenu'),
+      submenu: [
+        {
+          label: 'Subitem 1',
+          click: () => console.log('Click on subitem 1')
+        },
+        {
+          type: 'separator'
+        }
+      ]
+    }));
+
+    menu.append(new MenuItem({
+      label: 'Item 2',
+      submenu: [
+        {
+          label: 'Subitem checkbox',
+          type: 'checkbox',
+          checked: true
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Subitem with submenu',
+          submenu: [
+            {
+              label: 'Submenu &item 2',
+              accelerator: 'Ctrl+T',
+            }
+          ]
+        }
+      ]
+    }));
+
     new customTitlebar.Titlebar({
       backgroundColor: customTitlebar.Color.fromHex('#2f3241'),
       icon: url.format(path.join(__dirname, '/images', '/icon.png')),
+      menu
     });
   }
 
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,4 @@
 export * from './titlebar';
 export * from './themebar';
 export * from './vs/base/common/color';
+export * from './vs/base/common/platform';
